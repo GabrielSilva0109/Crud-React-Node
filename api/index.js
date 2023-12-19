74a7d5d42a2b845aaec4eb47e126e936deb72c6d
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2023-12-19 09:05:05 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
+//EXPRESS Simplifica a criação dos Servidores usando NODE.JS
 import express from "express"
+
+//USERROUTES São as rotas HTTP "GET" "PUT" "POST" "DELETE"
+import userRoutes from "./routes/users.js"
+
+//CORS Cuida da segurança das requisições
+import cors from "cors"
 
 const app = express()
 
-app.use(express.json());
+app.use(express.json())
+
 app.use(cors())
 
 app.use("/", userRoutes)
