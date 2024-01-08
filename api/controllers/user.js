87ed99ea10d864fc37af78cbe31fb746eb6bb670
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-01-08 11:22:37 -0300

    Update user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -12,7 +12,7 @@ export const getUsers = (_, res) => {
 
 export const addUser = (req, res) => {
   const q =
-    "INSERT INTO usuarios(`nome`, `email`, `fone`, `data_nascimento`) VALUES(?)";
+    "INSERT INTO usuarios (`nome`, `email`, `fone`, `data_nasc`) VALUES (?, ?, ?, ?)";
 
   const values = [
     req.body.nome,
@@ -21,12 +21,29 @@ export const addUser = (req, res) => {
     req.body.data_nascimento,
   ];
 
-  db.query(q, [values], (err) => {
-    if (err) return res.json(err);
+  db.query(q, values, (err, result) => {
+    if (err) {
+      console.error("Erro ao inserir usuário:", err);
+      return res.status(500).json({ message: "Erro ao criar usuário." });
+    }
 
-    return res.status(200).json("Usuário criado com sucesso.");
+    const insertedUserId = result.insertId; // Obtém o ID do usuário inserido
+
+    db.query(
+      `SELECT * FROM usuarios WHERE id = ${insertedUserId}`,
+      (err, data) => {
+        if (err) {
+          console.error("Erro ao buscar usuário inserido:", err);
+          return res.status(500).json({ message: "Erro ao buscar usuário." });
+        }
+
+        return res.status(200).json(data[0]); // Retorna os dados do usuário inserido
+      }
+    );
   });
 };
+
+
 
 export const updateUser = (req, res) => {
   const q =
